28e289d1e4aec1d62933df68ee8a9464767402cf
Andrii Myronchenko <deymos2042@gmail.com>
2023-11-26 08:45:17 +0100

    Fix exports of components

--- gamestore-frontend/src/app/layouts/baseLayout.tsx
+++ gamestore-frontend/src/app/layouts/baseLayout.tsx
@@ -4,12 +4,12 @@ import {Layout} from "@/shared/ui/layout/Layout";
 import {Header} from "@/widgets/Header";
 
 type BaseLayoutProps = {
-  children: ReactNode
+  children?: ReactNode
 }
 export const BaseLayout = ({children}: BaseLayoutProps) => {
   return (
     <Layout headerSlot={<Header/>}>
-      {children}
+      {children ? children : null}
     </Layout>
   );
 };
